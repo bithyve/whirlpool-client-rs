09efb04dc844ca1e4dd3e61be259ebff5a7ee719
Shashank Shinde <shashank@cakesofttech.com>
2023-04-28 17:44:28 +0530

    partner id updated

--- src/tx0.rs
+++ src/tx0.rs
@@ -379,7 +379,7 @@ impl Tx0Data {
 
         let request = RequestPayload {
             scode,
-            partner_id: std::option_env!("WPID").unwrap_or("FREESIDE"),
+            partner_id: std::option_env!("WPID").unwrap_or("WHIRLPOOL_HEXA"),
         };
 
         http::Request {
